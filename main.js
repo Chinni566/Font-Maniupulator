251cd43ece0418b5ad1fd8c0e235332e5ba89cff
Neeraj <78613839+Chinni566@users.noreply.github.com>
2022-07-10 12:40:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
-function setup () {
-    canvas=createCanvas(550,550)
-    canvas.position(560,150)
-}
+function setup(){
+    video= createCapture(VIDEO);
+    video.size(550,500);
+    
+     canvas= createCanvas(500,500);
+     canvas.position(560,130);
+    
+     poseNet= ml5.poseNet(video,modelLoaded);
+     poseNet.on('pose', gotPoses);
+    }
+    
+    function gotPoses(){
+        if(results.length > 0);
+        {
+       console.log(results);
+        }
+    }
+    
+    function draw(){
+        background('#000000')
+    }
+     
+    function modelLoaded(){
+        console.log('PoseNet is initialized');
+    
+    }
